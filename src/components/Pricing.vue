9e731bc8896aa40cd0aa6bca9e134514f465b16e
zemo0 <ivanzeman3@gmail.com>
2021-06-06 22:51:49 +0200

    Initial commit

---
+++ src/components/Pricing.vue
@@ -0,0 +1,68 @@
+<template>
+    <main id="main">
+
+    <!-- ======= Pricing Section ======= -->
+    <section class="pricing section-bg" data-aos="fade-up">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Cjenik</h2>
+          <p>Najbolji način na svijetu za dogovoriti sastanak</p>
+        </div>
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-4 box">
+            <h3>Free</h3>
+            <h4>0kn<span>po mjesecu</span></h4>
+            <ul>
+              <li><i class="bx bx-check"></i> Osnovni planer</li>
+              <li class="na"><i class="bx bx-x"></i> <span>Sinkronizacija sastanka na kalendar</span></li>
+              <li class="na"><i class="bx bx-x"></i> <span>1-1 sastanci</span></li>
+            </ul>
+            <a href="#" class="get-started-btn">Započni</a>
+          </div>
+
+          <div class="col-lg-4 box featured">
+            <h3>Pro</h3>
+            <h4>100kn<span>po mjesecu</span></h4>
+            <ul>
+              <li><i class="bx bx-check"></i> Bez reklama</li>
+              <li><i class="bx bx-check"></i> Sinkronizacija sastanka na kalendar</li>
+              <li><i class="bx bx-check"></i> 1-1 sastanci</li>
+              <li><i class="bx bx-check"></i> Podrška za korisnike</li>
+              <li><i class="bx bx-check"></i> Integracija u Microsoft Teams</li>
+            </ul>
+            <a href="#" class="get-started-btn">Započni</a>
+          </div>
+
+          <div class="col-lg-4 box">
+            <h3>Tim</h3>
+            <h4>300kn<span>po mjesecu</span></h4>
+            <ul>
+              <li><i class="bx bx-check"></i> Prilagođena naplata i pravni uvjeti</li>
+              <li><i class="bx bx-check"></i> Prioritetna podrška</li>
+              <li><i class="bx bx-check"></i> Zajamčeno vrijeme odziva</li>
+              <li><i class="bx bx-check"></i> Obuka korisnika</li>
+              <li><i class="bx bx-check"></i> Ugovorna obveza o pružanju usluga</li>
+            </ul>
+            <a href="#" class="get-started-btn">Započni</a>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Pricing Section -->
+
+  </main><!-- End #main -->
+</template>
+
+<script>
+export default {
+    name: "Pricing"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Router from 'vue-router'
+import Vue from 'vue'
+
+
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+import Contact from '../components/Contact.vue'
+import Pricing from '../components/Pricing.vue'
+import Services from '../components/Services.vue'
+
+Vue.use(Router)
+const router= new Router({
+  routes: [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/services',
+    name: 'services',
+    component: Services
+  },
+  {
+    path: '/pricing',
+    name: 'pricing',
+    component: Pricing
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  },
+],
+mode:'history'
+})
+
+export default router
